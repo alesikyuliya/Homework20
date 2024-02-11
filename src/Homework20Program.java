789ca3a78e73f8a057d4503086211e6c145af6f5
Yuliya Alesik <alesikyuliya@outlook.com>
2024-02-11 22:39:30 +0100

    Second Commit

--- src/Homework20Program.java
+++ src/Homework20Program.java
@@ -1,7 +1,9 @@
 public class Homework20Program {
     public static void main(String[] args) {
-        Client alice = new Client("Alice", "White", 11, "February", 1996, "alice@gmail.com", 532125365L, 53, "60/120", 500);
-        Client bob = new Client("Bob", "Johnnes", 11, "February", 1992, "bob@gmail.com", 534445365L, 72, "70/122", 900);
+        Client alice = new Client("Alice", "White", 11, "February", 1996,
+                "alice@gmail.com", 532125365L, 53, "60/120", 500);
+        Client bob = new Client("Bob", "Johnnes", 11, "February", 1992,
+                "bob@gmail.com", 534445365L, 72, "70/122", 900);
         alice.printAccount();
         bob.printAccount();
         System.out.println("_______________________________");
